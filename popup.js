386962df1f4ab9a183551f3330ea9ace42d8d739
Alexander Alemayhu <alexander@alemayhu.com>
2025-04-06 03:44:35 +0200

    feat: add the open all links pop up
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

---
+++ popup.js
@@ -0,0 +1,15 @@
+document.getElementById("openLinks").addEventListener("click", async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: openAllLinks
+  });
+});
+
+function openAllLinks() {
+  const links = Array.from(document.querySelectorAll("a[href]"))
+    .map(a => a.href)
+    .filter(href => href.startsWith("http"));
+
+  links.forEach(link => window.open(link, '_blank'));
+}
