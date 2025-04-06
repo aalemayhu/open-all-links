092addfb645d5d51b4a93ad2c8116ea951e43833
Alexander Alemayhu <alexander@alemayhu.com>
2025-04-06 04:50:26 +0200

    Fix CI workflow: remove non-existent eslint-plugin-chrome and fix linting issues

--- popup.js
+++ popup.js
@@ -8,8 +8,8 @@ document.getElementById("openLinks").addEventListener("click", async () => {
 
 function openAllLinks() {
   const links = Array.from(document.querySelectorAll("a[href]"))
-    .map(a => a.href)
-    .filter(href => href.startsWith("http"));
+    .map((a) => a.href)
+    .filter((href) => href.startsWith("http"));
 
-  links.forEach(link => window.open(link, '_blank'));
+  links.forEach((link) => window.open(link, "_blank"));
 }
